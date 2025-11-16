8d261b983ade6093fac78f921de891b163cc87cb
Abhi <abhii.mailboxx@gmai.com>
2025-11-16 20:34:26 +1100

    Using optparse to integrate arguments and help function in our tool

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = input("interface > ")
-new_mac = input("new MAC > ")
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
+
 print(f"[+] Changing MAC address for", interface, "to", new_mac)
-
-
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
-
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])

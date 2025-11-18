52339192bb5b4b2b5e08b524360b9d7d0309c416
AbhiWiz-gif <abhii.mailboxx@gmail.com>
2025-11-19 09:22:56 +1100

    Sending & receiving Packets using Scapy

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -4,13 +4,14 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
-    arp_request_broadcast = broadcast/arp_request
 
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    answered_list,unanswered_list = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered_list.summary())
+
+    # print(arp_request_broadcast.summary())
+    # arp_request_broadcast.show()
     # print(arp_request.summary())
     # print(broadcast.summary())
     # scapy.ls(scapy.ARP())
@@ -18,4 +19,4 @@ def scan(ip):
 
 
 
-scan("192.168.1.1")
+scan("192.168.1.1/24")

81a18760fbf24923c3831e18bdbf29cda988b770
AbhiWiz-gif <abhii.mailboxx@gmail.com>
2025-11-22 08:34:41 +1100

    Iterating over list to clean the packet and imporve program output

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -7,16 +7,18 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 
     arp_request_broadcast = broadcast/arp_request
-    answered_list,unanswered_list = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered_list.summary())
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+    print("IP\t\t\tMAC Address\n-----------------------------------------")
+    for element in answered_list:
+        print(element[1].psrc + "\t\t"+ element[1].hwsrc)
+        print("-----------------------------------------")
+        
     # print(arp_request_broadcast.summary())
     # arp_request_broadcast.show()
     # print(arp_request.summary())
     # print(broadcast.summary())
     # scapy.ls(scapy.ARP())
     # scapy.ls(scapy.Ether())
-
-
 
 scan("192.168.1.1/24")

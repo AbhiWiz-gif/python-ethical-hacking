48d9c6a7318b77fd15b973b0b76d42e453caad3f
AbhiWiz-gif <abhii.mailboxx@gmail.com>
2025-11-22 09:22:26 +1100

    Finalizing scanner using opt parser with -t/ target option form command line

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest = "target", help = "IP address range to do scan")
+    (options, arguments) = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify an IP range, use --help fo more")
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -9,16 +18,24 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print("IP\t\t\tMAC Address\n-----------------------------------------")
+    client_list = []
     for element in answered_list:
-        print(element[1].psrc + "\t\t"+ element[1].hwsrc)
-        print("-----------------------------------------")
-        
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)
+    return(client_list)
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n-----------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
     # print(arp_request_broadcast.summary())
     # arp_request_broadcast.show()
-    # print(arp_request.summary())
+    # print(arp_request.summary()) 
     # print(broadcast.summary())
     # scapy.ls(scapy.ARP())
     # scapy.ls(scapy.Ether())
 
-scan("192.168.1.1/24")
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
